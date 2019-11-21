1f327c45d9683c3ea3d6d81cd39757bbbcab4e35
ABUZAR MAHMOODI <mahmoodi.abuzar@gmail.com>
2019-11-21 16:46:29 -0500

    this is another commit

---
+++ src/main/java/secondClass.java
@@ -0,0 +1,7 @@
+public class secondClass {
+    public static void main(String[] args) {
+        System.out.println("Cybertek Rock");
+        System.out.println("I dont care If I am in school all day");
+
+    }
+}
