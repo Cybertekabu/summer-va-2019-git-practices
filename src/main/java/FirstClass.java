88cca37ef875aedef0730e8d7a942fab5b14352c
Cybertekabu <55052908+Cybertekabu@users.noreply.github.com>
2019-12-05 14:40:24 -0500

    Update FirstClass.java

--- src/main/java/FirstClass.java
+++ src/main/java/FirstClass.java
@@ -1,6 +1,7 @@
 public class FirstClass {
     public static void main(String[] args) {
-        System.out.println("Hhi world or hello  Mr.Seyfo");
+        System.out.println("i world or hello  Mr.Seyfo");
+        
 
     }
 }
