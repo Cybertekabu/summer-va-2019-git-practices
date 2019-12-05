1ea5a98fa238130da38e532bac8a7843c70ce7b0
abuzarmahmoodi <mahmoodi.abuzar@gmail.com>
2019-12-05 16:30:59 -0500

    fixed conflict between

--- src/main/java/FirstClass.java
+++ src/main/java/FirstClass.java
@@ -1,7 +1,7 @@
 public class FirstClass {
     public static void main(String[] args) {
         System.out.println("i world or hello  Mr.Seyffffoooooo");
-        
+        System.out.println("Abuzar Mahmoodi");
 
     }
 }
